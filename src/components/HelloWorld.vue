1acd7f988490766bccdad58fa8fe60227ec42b66
shvachkoelena <shvachko.elenaa@gmail.com>
2024-11-17 22:02:16 +0300

    commit lesson1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,22 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <div>
+        <h2>Мой компонент</h2>
+        <button @click="toggleText">{{ showText ? 'Скрыть' : 'Показать' }}</button>
+        <!-- <button @click="showText = !showText">{{ showText ? 'Скрыть' : 'Показать' }}</button> -->
+        <p v-show="showText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugit a ducimus totam minima esse voluptates eos repellendus nesciunt pariatur nobis placeat rem ipsa animi quidem, fugiat reprehenderit in exercitationem.</p>
+    </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<script setup>
+import { ref } from 'vue';
+
+const showText = ref(false);
+const toggleText = () => {
+    showText.value = !showText.value
 }
+
+</script>
+
+<style lang="scss" scoped>
+
 </style>
